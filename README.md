f8e7b3923b76a95721cc763c7f288f01ecbac514
Nata Nael <natanael280198@gmail.com>
2024-03-14 00:23:54 +0700

    feat: CountingDays section

--- README.md
+++ README.md
@@ -33,3 +33,4 @@ export default {
 
 - https://www.bridestory.com/_next/image?url=https%3A%2F%2Falexandra.bridestory.com%2Fimage%2Fupload%2Fdpr_1.0%2Cf_auto%2Cfl_progressive%2Cq_80%2Fv1%2Fassets%2Fbranches-GWZDtNSgR.png&w=96&q=75
 - https://www.bridestory.com/_next/image?url=https%3A%2F%2Falexandra.bridestory.com%2Fimage%2Fupload%2Fdpr_1.0%2Cf_auto%2Cfl_progressive%2Cq_80%2Fv1%2Fassets%2Fgroup-282x-n71nYQ2ew.png&w=384&q=75
+- https://alexandra.bridestory.com/image/upload/f_webp/v1/assets/maria-orlova-12yiylq49bi-unsplash2x-Hyj7ryFt6.webp
--- src/components/CountingDays.tsx
+++ src/components/CountingDays.tsx
@@ -1,5 +1,138 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  background-image: url(https://alexandra.bridestory.com/image/upload/f_webp/v1/assets/maria-orlova-12yiylq49bi-unsplash2x-Hyj7ryFt6.webp);
+  height: 1000px;
+  object-fit: cover;
+  background-position: center;
+`;
+
+const SectionContainer = styled.div`
+  width: 50%;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4em;
+  flex-direction: column;
+`;
+
+const SectionHeader = styled.h1`
+  font-size: 5.5rem;
+  font-family: "Alex Brush", cursive;
+  font-weight: 400;
+  font-style: normal;
+  color: white;
+
+  span {
+    font-size: 3rem;
+    font-family: "Alex Brush", cursive;
+    font-weight: 400;
+    font-style: normal;
+  }
+`;
+
+const DateText = styled.div`
+  font-size: 1.3rem;
+  color: white;
+  font-weight: 500;
+  margin-top: 1em;
+`;
+
+const ScheduleDiv = styled.div<{ $reversed?: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  margin: 3em;
+  gap: 5em;
+  flex-direction: ${(props) =>
+    props.$reversed === true ? "row-reverse" : "row"};
+
+  img {
+    width: 270px;
+    height: 270px;
+    object-fit: cover;
+    border-radius: 1em;
+  }
+
+  .schedule-description {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1em;
+    font-size: 0.8;
+    width: 45%;
+
+    h1 {
+      color: white;
+      letter-spacing: 6px;
+    }
+
+    .date,
+    .time,
+    .address {
+      color: white;
+      font-weight: 400;
+    }
+
+    button {
+      background-color: transparent;
+      border: 1px solid white;
+      color: white;
+      width: fit-content;
+      margin: 0 auto;
+      padding: 1em 3em;
+      border-radius: 1em;
+      cursor: pointer;
+      font-weight: bold;
+    }
+  }
+`;
+
 const CountingDays = () => {
-  return <div>CountingDays</div>;
+  return (
+    <Section>
+      <SectionContainer>
+        <SectionHeader>
+          Counting <span>the</span> Days
+        </SectionHeader>
+        <DateText>Saturday, 03 February 2024</DateText>
+
+        <ScheduleDiv>
+          <img
+            src='https://unsplash.com/photos/w5_xJ13Ryf0/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MzB8fGJyaWRlJTIwYW5kJTIwZ3Jvb218ZW58MHx8fHwxNzEwMjkxMDQ4fDA&force=true&w=640'
+            alt='bride and groom image'
+          />
+          <div className='schedule-description'>
+            <h1>AKAD NIKAH</h1>
+            <div className='date'>20 February 2023</div>
+            <div className='time'>01:00 - 02:00 WIB</div>
+            <div className='address'>
+              Assembly Hall Menara Mandiri Lt.10 Jl. Jenderal Sudirman No.Kav
+              54-55, Senayan - Jakarta Selatan
+            </div>
+            <button>Open in Maps</button>
+          </div>
+        </ScheduleDiv>
+        <ScheduleDiv $reversed={true}>
+          <img
+            src='https://unsplash.com/photos/w5_xJ13Ryf0/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MzB8fGJyaWRlJTIwYW5kJTIwZ3Jvb218ZW58MHx8fHwxNzEwMjkxMDQ4fDA&force=true&w=640'
+            alt='bride and groom image'
+          />
+          <div className='schedule-description'>
+            <h1>RESEPSI</h1>
+            <div className='date'>20 February 2023</div>
+            <div className='time'>19:00 - 21:00 WIB</div>
+            <div className='address'>
+              Assembly Hall Menara Mandiri Lt.10 Jl. Jenderal Sudirman No.Kav
+              54-55, Senayan - Jakarta Selatan
+            </div>
+            <button>Open in Maps</button>
+          </div>
+        </ScheduleDiv>
+      </SectionContainer>
+    </Section>
+  );
 };
 
 export default CountingDays;
